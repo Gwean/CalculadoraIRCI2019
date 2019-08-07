53f4ce92e7b305e47bd4edff66c1dffe44dee54d
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-08-07 17:18:32 -0300

    Agregada sgn al functions.s

--- functions.s
+++ functions.s
@@ -3,6 +3,8 @@ hexa: .ascii "0123456789ABCDEF"
 res: .asciiz "00000000\n"	# Colocar tantos 0s como digitos
 				# hexadecimales tenga la respuesta.
 
+dbl: .space 8
+
 .text			# incluir esto en el .text del codigo
 
 # $a0 -> Numero a convertir.
@@ -30,3 +32,16 @@ E1:   lw  $a2, 0($sp)
       lw  $a0, 8($sp)
       addi $sp, $sp, 12
       jr $ra
+
+# $a0 -> direccion del double
+# $a1 -> donde quiero el signo
+sgn:	addi $sp, $sp, -8
+	sw   $a0, 4($sp)
+	sw   $a1, 0($sp)	# Apilo en el stack
+	addi $a0, $a0, 4	# Avanzo a la primer mitad del double
+	lw $a1, ($a0)		# Consigo en $a1 la primer mitad
+	srl  $t1, $t1, 31	# Llevo el primer bit, el signo, al final
+	lw   $a0, 4($sp)
+	lw   $a1, 0($sp)
+	addi $sp, $sp, 8	# Popeo en el stack
+	jr $ra
