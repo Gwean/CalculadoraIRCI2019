d3b866c2b19f6c20954901c74c92dff12b1a7dc4
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-08-07 17:17:09 -0300

    Convertida signo.s a una funcion

--- signo.s
+++ signo.s
@@ -1,22 +1,27 @@
-.data
-float: .space 8
+.data			# HAY que agregar esto al .data
+dbl: .space 8
 
 .text
 
-main:
+main:			# Esto es solo un ejemplo
 
 li $v0, 7
 syscall
 
-la $t0, float		# En $t0 tengo la direccion del float
+la $a0, dbl		# En $a0 tengo la direccion del double
+s.d $f0, ($a0)		# Cuidado, se almacena la primera mitad despues
+			# de la segunda
 
-s.d $f0, ($t0)		# Se almacena la primera mitad despues de la segunda
 
-addi $t0, $t0, 4	# Avanzo al proximo word
-
-lw $t1, ($t0)
-
-srl  $t1, $t1, 31
-
-li $v0, 10
-syscall
+# $a0 -> direccion del double
+# $a1 -> donde quiero el signo
+sgn:	addi $sp, $sp, -8
+	sw   $a0, 4($sp)
+	sw   $a1, 0($sp)	# Apilo en el stack
+	addi $a0, $a0, 4	# Avanzo a la primer mitad del double
+	lw $a1, ($a0)		# Consigo en $a1 la primer mitad
+	srl  $t1, $t1, 31	# Llevo el primer bit, el signo, al final
+	lw   $a0, 4($sp)
+	lw   $a1, 0($sp)
+	addi $sp, $sp, 8	# Popeo en el stack
+	jr $ra
